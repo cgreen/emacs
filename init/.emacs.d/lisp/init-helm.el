e4eb73b7ea972679540509fc2334ea184422d33e
Craig Green <craigree@microsoft.com>
2018-10-16 16:12:54 -0700

    Add Helm to selected packages and rebind M-x to use helm-M-x.

--- init/.emacs.d/lisp/init-helm.el
+++ init/.emacs.d/lisp/init-helm.el
@@ -1,1 +1,9 @@
 (require 'helm-config)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+;; Workaround a bug that causes Helm buffers to open slowly in emacs 26
+;; https://github.com/emacs-helm/helm/issues/1976
+;;(when (= emacs-major-version 26)
+;;  (setq x-wait-for-event-timeout nil))
+
